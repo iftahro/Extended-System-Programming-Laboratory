2fca46e05778dc0fc62b3d2e8935a0506f61feba
iftahro <ifttah@gmail.com>
2026-03-23 19:58:41 +0200

    Del comments

--- Lab A/encoder.c
+++ Lab A/encoder.c
@@ -1,84 +1,60 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* Global variables */
 FILE *infile;
 FILE *outfile;
 unsigned char password[] = "my_password1";
 
-/* Vigenere Cipher globals */
-char *key = "A"; /* Default key: 'A' means shift by 0 */
-int key_dir = 1;   /* 1 for addition (+V), -1 for subtraction (-V) */
-int key_idx = 0;   /* Current position in the key */
+char *key = "A"; 
+int key_dir = 1;   
+int key_idx = 0;   
 
-/* Encode function for Part 2 and 3: applies Vigenere cipher */
 int encode(int c) {
     int shift;
     int new_c = c;
 
-    /* Check if character is a letter using manual comparison */
+    /* Process only alphabetic characters */
     if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')) {
         
-        /* Calculate shift value (0 to 25) and apply direction (+ or -) */
         shift = (key[key_idx] - 'A') * key_dir;
 
-        /* Apply shift for lowercase letters */
         if (c >= 'a' && c <= 'z') {
             new_c = c + shift;
-            /* Handle wrap-around */
-            while (new_c > 'z') {
-                new_c -= 26;
-            }
-            while (new_c < 'a') {
-                new_c += 26;
-            }
+            while (new_c > 'z') new_c -= 26;
+            while (new_c < 'a') new_c += 26;
         } 
-        /* Apply shift for uppercase letters */
         else if (c >= 'A' && c <= 'Z') {
             new_c = c + shift;
-            /* Handle wrap-around */
-            while (new_c > 'Z') {
-                new_c -= 26;
-            }
-            while (new_c < 'A') {
-                new_c += 26;
-            }
+            while (new_c > 'Z') new_c -= 26;
+            while (new_c < 'A') new_c += 26;
         }
 
-        /* Advance key index only when a letter is encoded */
         key_idx++;
         
-        /* Reset key index if we reached the end of the key string */
         if (key[key_idx] == '\0') {
             key_idx = 0;
         }
     }
     
-    /* Non-letter characters skip the if-block and return unchanged */
     return new_c;
 }
 
 int main(int argc, char **argv) {
     int i;
-    int debug_mode = 1; /* Debug mode is ON by default */
+    int debug_mode = 1; 
     int c;
 
-    /* Initialize file pointers to standard streams */
     infile = stdin;
     outfile = stdout;
 
-    /* Parse command-line arguments starting from 0 to include program name */
     for (i = 0; i < argc; i++) {
-        /* Print the argument to stderr if debug mode is currently ON */
         if (debug_mode) {
             fprintf(stderr, "%s\n", argv[i]);
         }
 
-        /* Check if the argument is "-D" to turn debug mode OFF */
         if (argv[i][0] == '-' && argv[i][1] == 'D' && argv[i][2] == '\0') {
             debug_mode = 0;
         } 
-        /* Check if the argument starts with "+D" to turn debug mode ON */
         else if (argv[i][0] == '+' && argv[i][1] == 'D') {
             int j = 0;
             int match = 1;
@@ -91,62 +67,42 @@ int main(int argc, char **argv) {
                 j++;
             }
             
-            if (match) {
-                debug_mode = 1;
-            }
+            if (match) debug_mode = 1;
         }
-        /* Check if the argument is an encoding key (+V... or -V...) */
         else if ((argv[i][0] == '+' || argv[i][0] == '-') && argv[i][1] == 'V') {
-            if (argv[i][0] == '+') {
-                key_dir = 1;
-            } else {
-                key_dir = -1;
-            }
-            /* Set the key pointer to start exactly after the 'V' */
+            if (argv[i][0] == '+') key_dir = 1;
+            else key_dir = -1;
+            
             key = &argv[i][2];
         }
-        /* Part 3: Check for input file argument (-ifilename) */
         else if (argv[i][0] == '-' && argv[i][1] == 'i') {
-            /* Open the file for reading. The filename starts after the "-i" */
             infile = fopen(&argv[i][2], "r");
             if (infile == NULL) {
                 fprintf(stderr, "Error: Cannot open input file %s\n", &argv[i][2]);
-                return 1; /* Exit with error code */
+                return 1; 
             }
         }
-        /* Part 3: Check for output file argument (-ofilename) */
         else if (argv[i][0] == '-' && argv[i][1] == 'o') {
-            /* Open the file for writing. The filename starts after the "-o" */
             outfile = fopen(&argv[i][2], "w");
             if (outfile == NULL) {
                 fprintf(stderr, "Error: Cannot open output file %s\n", &argv[i][2]);
-                return 1; /* Exit with error code */
+                return 1; 
             }
         }
     }
 
-    /* Input processing loop */
+    /* Read and encode input character by character */
     while (1) {
         c = fgetc(infile);
         
-        /* Break the loop if EOF is detected */
-        if (feof(infile)) {
-            break;
-        }
+        if (feof(infile)) break;
         
         c = encode(c);
         fputc(c, outfile);
     }
 
-    /* Close the output stream if it's not stdout */
-    if (outfile != stdout) {
-        fclose(outfile);
-    }
-    
-    /* Close the input stream if it's not stdin */
-    if (infile != stdin) {
-        fclose(infile);
-    }
+    if (outfile != stdout) fclose(outfile);
+    if (infile != stdin) fclose(infile);
 
     return 0;
 }
