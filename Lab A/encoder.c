c72e8e2f52a4ee32dfc68a25503abe9db80d2e86
iftahro <ifttah@gmail.com>
2026-03-23 19:49:19 +0200

    Part 2

--- Lab A/encoder.c
+++ Lab A/encoder.c
@@ -1,14 +1,61 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* Global variables as recommended */
+/* Global variables */
 FILE *infile;
 FILE *outfile;
 unsigned char password[] = "my_password1";
 
-/* Encode function for Part 1: returns the character unchanged */
+/* Vigenere Cipher globals */
+char *key = "A"; /* Default key: 'A' means shift by 0 */
+int key_dir = 1;   /* 1 for addition (+V), -1 for subtraction (-V) */
+int key_idx = 0;   /* Current position in the key */
+
+/* Encode function for Part 2: applies Vigenere cipher */
 int encode(int c) {
-    return c;
+    int shift;
+    int new_c = c;
+
+    /* Check if character is a letter using manual comparison */
+    if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')) {
+        
+        /* Calculate shift value (0 to 25) and apply direction (+ or -) */
+        shift = (key[key_idx] - 'A') * key_dir;
+
+        /* Apply shift for lowercase letters */
+        if (c >= 'a' && c <= 'z') {
+            new_c = c + shift;
+            /* Handle wrap-around */
+            while (new_c > 'z') {
+                new_c -= 26;
+            }
+            while (new_c < 'a') {
+                new_c += 26;
+            }
+        } 
+        /* Apply shift for uppercase letters */
+        else if (c >= 'A' && c <= 'Z') {
+            new_c = c + shift;
+            /* Handle wrap-around */
+            while (new_c > 'Z') {
+                new_c -= 26;
+            }
+            while (new_c < 'A') {
+                new_c += 26;
+            }
+        }
+
+        /* Advance key index only when a letter is encoded */
+        key_idx++;
+        
+        /* Reset key index if we reached the end of the key string */
+        if (key[key_idx] == '\0') {
+            key_idx = 0;
+        }
+    }
+    
+    /* Non-letter characters skip the if-block and return unchanged */
+    return new_c;
 }
 
 int main(int argc, char **argv) {
@@ -20,8 +67,8 @@ int main(int argc, char **argv) {
     infile = stdin;
     outfile = stdout;
 
-    /* Parse command-line arguments */
-    for (i = 1; i < argc; i++) {
+    /* Parse command-line arguments starting from 0 to include program name */
+    for (i = 0; i < argc; i++) {
         /* Print the argument to stderr if debug mode is currently ON */
         if (debug_mode) {
             fprintf(stderr, "%s\n", argv[i]);
@@ -36,7 +83,6 @@ int main(int argc, char **argv) {
             int j = 0;
             int match = 1;
             
-            /* Compare the rest of the argument with the password character by character */
             while (password[j] != '\0' || argv[i][j + 2] != '\0') {
                 if (password[j] != argv[i][j + 2]) {
                     match = 0;
@@ -49,9 +95,19 @@ int main(int argc, char **argv) {
                 debug_mode = 1;
             }
         }
+        /* Check if the argument is an encoding key (+V... or -V...) */
+        else if ((argv[i][0] == '+' || argv[i][0] == '-') && argv[i][1] == 'V') {
+            if (argv[i][0] == '+') {
+                key_dir = 1;
+            } else {
+                key_dir = -1;
+            }
+            /* Set the key pointer to start exactly after the 'V' */
+            key = &argv[i][2];
+        }
     }
 
-    /* Input echoing loop */
+    /* Input processing loop */
     while (1) {
         c = fgetc(infile);
         
@@ -69,7 +125,7 @@ int main(int argc, char **argv) {
         fclose(outfile);
     }
     
-    /* Close the input stream if it's not stdin (preparation for Part 3) */
+    /* Close the input stream if it's not stdin */
     if (infile != stdin) {
         fclose(infile);
     }
