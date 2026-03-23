3b0f2e67f8f823dc998dbb13d4b42ac4eb42327f
iftahro <ifttah@gmail.com>
2026-03-23 20:16:19 +0200

    Fix

--- Lab A/encoder.c
+++ Lab A/encoder.c
@@ -12,12 +12,9 @@ int enc_idx = 0;
 int encode(int c) {
     int shift;
     int new_c = c;
-
     /* Process only alphabetic characters */
     if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')) {
-        
         shift = (encoding_string[enc_idx] - 'A') * shift_direction;
-
         if (c >= 'a' && c <= 'z') {
             new_c = c + shift;
             while (new_c > 'z') new_c -= 26;
@@ -28,14 +25,11 @@ int encode(int c) {
             while (new_c > 'Z') new_c -= 26;
             while (new_c < 'A') new_c += 26;
         }
-
         enc_idx++;
-        
         if (encoding_string[enc_idx] == '\0') {
             enc_idx = 0;
         }
     }
-    
     return new_c;
 }
 
@@ -43,15 +37,13 @@ int main(int argc, char **argv) {
     int i;
     int is_debug_on = 1; 
     int c;
-
     in_file = stdin;
     out_file = stdout;
-
+    /* Handle program arguments */
     for (i = 0; i < argc; i++) {
         if (is_debug_on) {
             fprintf(stderr, "%s\n", argv[i]);
         }
-
         if (argv[i][0] == '-' && argv[i][1] == 'D' && argv[i][2] == '\0') {
             is_debug_on = 0;
         } 
@@ -66,13 +58,17 @@ int main(int argc, char **argv) {
                 }
                 j++;
             }
-            
-            if (match) is_debug_on = 1;
+            if (match) {
+                is_debug_on = 1
+            };
         }
         else if ((argv[i][0] == '+' || argv[i][0] == '-') && argv[i][1] == 'V') {
-            if (argv[i][0] == '+') shift_direction = 1;
-            else shift_direction = -1;
-            
+            if (argv[i][0] == '+') {
+                shift_direction = 1
+            };
+            else {
+                shift_direction = -1
+            };
             encoding_string = &argv[i][2];
         }
         else if (argv[i][0] == '-' && argv[i][1] == 'i') {
@@ -90,19 +86,20 @@ int main(int argc, char **argv) {
             }
         }
     }
-
     /* Read and encode input character by character */
     while (1) {
         c = fgetc(in_file);
-        
-        if (feof(in_file)) break;
-        
+        if (feof(in_file)) {
+            break
+        };
         c = encode(c);
         fputc(c, out_file);
     }
-
-    if (out_file != stdout) fclose(out_file);
-    if (in_file != stdin) fclose(in_file);
-
+    if (out_file != stdout) {
+        fclose(out_file)
+    };
+    if (in_file != stdin) {
+        fclose(in_file)
+    };
     return 0;
 }
