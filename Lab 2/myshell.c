c3d0d3ec85f66ccd0b73c7a9c9d53c81e274c457
iftahro <ifttah@gmail.com>
2026-05-02 16:53:54 +0300

    Lab 2 myshell

---
+++ Lab 2/myshell.c
@@ -0,0 +1,137 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <linux/limits.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <fcntl.h>
+#include "LineParser.h"
+
+#define MAX_INPUT_SIZE 2048
+
+int debug_mode = 0;
+
+void execute(cmdLine *pCmdLine) {
+    if (strcmp(pCmdLine->arguments[0], "cd") == 0) {
+        if (chdir(pCmdLine->arguments[1]) == -1) {
+            perror("cd failed");
+        }
+        return;
+    }
+
+    if (strcmp(pCmdLine->arguments[0], "stop") == 0) {
+        if (kill(atoi(pCmdLine->arguments[1]), SIGSTOP) == -1) perror("kill failed");
+        return;
+    }
+    if (strcmp(pCmdLine->arguments[0], "wakeup") == 0) {
+        if (kill(atoi(pCmdLine->arguments[1]), SIGCONT) == -1) perror("kill failed");
+        return;
+    }
+    if (strcmp(pCmdLine->arguments[0], "ice") == 0) {
+        if (kill(atoi(pCmdLine->arguments[1]), SIGINT) == -1) perror("kill failed");
+        return;
+    }
+    if (strcmp(pCmdLine->arguments[0], "nuke") == 0) {
+        pid_t pgid = atoi(pCmdLine->arguments[1]);
+        if (kill(-pgid, SIGKILL) == -1) perror("kill failed");
+        return;
+    }
+
+    pid_t pid = fork();
+
+    if (pid == 0) {
+        if (debug_mode) {
+            fprintf(stderr, "PID: %d\n", getpid());
+            fprintf(stderr, "Executing command: %s\n", pCmdLine->arguments[0]);
+            if (pCmdLine->blocking == 1) {
+                fprintf(stderr, "Execution status: Foreground\n");
+            } else {
+                fprintf(stderr, "Execution status: Background\n");
+            }
+        }
+
+        // Input redirection
+        if (pCmdLine->inputRedirect != NULL) {
+            int fd_in = open(pCmdLine->inputRedirect, O_RDONLY);
+            if (fd_in == -1) {
+                perror("open input failed");
+                _exit(1);
+            }
+            if (dup2(fd_in, STDIN_FILENO) == -1) {
+                perror("dup2 input failed");
+                _exit(1);
+            }
+            close(fd_in);
+        }
+
+        // Output redirection
+        if (pCmdLine->outputRedirect != NULL) {
+            int fd_out = open(pCmdLine->outputRedirect, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+            if (fd_out == -1) {
+                perror("open output failed");
+                _exit(1);
+            }
+            if (dup2(fd_out, STDOUT_FILENO) == -1) {
+                perror("dup2 output failed");
+                _exit(1);
+            }
+            close(fd_out);
+        }
+
+        if (execvp(pCmdLine->arguments[0], pCmdLine->arguments) == -1) {
+            perror("Execution failed");
+            _exit(1);
+        }
+    } else if (pid > 0) {
+        if (pCmdLine->blocking == 1) {
+            waitpid(pid, NULL, 0);
+        }
+    } else {
+        perror("fork failed");
+    }
+}
+
+int main(int argc, char **argv) {
+    char cwd[PATH_MAX];
+    char input[MAX_INPUT_SIZE];
+    cmdLine *parsedLine;
+
+    for (int i = 1; i < argc; i++) {
+        if (strcmp(argv[i], "-d") == 0) {
+            debug_mode = 1;
+            break;
+        }
+    }
+
+    while (1) {
+        if (getcwd(cwd, sizeof(cwd)) != NULL) {
+            printf("%s> ", cwd);
+        } else {
+            perror("getcwd failed");
+            exit(1);
+        }
+
+        if (fgets(input, MAX_INPUT_SIZE, stdin) == NULL) {
+            break;
+        }
+
+        input[strcspn(input, "\n")] = 0;
+
+        if (strcmp(input, "quit") == 0) {
+            break;
+        }
+
+        if (strlen(input) == 0) {
+            continue;
+        }
+
+        parsedLine = parseCmdLines(input);
+        if (parsedLine != NULL) {
+            execute(parsedLine);
+            freeCmdLines(parsedLine);
+        }
+    }
+
+    return 0;
+}
